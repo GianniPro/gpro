0f24d8890e0cf117341d65a034f26031747c6b31
GianniPro <118400085+GianniPro@users.noreply.github.com>
2023-11-05 07:11:30 +0100

    Update search.js

--- search.js
+++ search.js
@@ -69,14 +69,14 @@ $( function() {
         {value:"Calculations On Friction on an Inclined Plane",link:'./Studies/Physics/frictional_force.html#friction-on-an-inclinded-plane'},
      
     ];
-    $( "#tags" ).complete({
+    $( "#tags" ).autocomplete({
       source: availableTags,
       select: function( event, ui ) {
         console.log(ui.item.link);
         window.location.replace(ui.item.link)
       }
     });
-    $( "#tag" ).complete({
+    $( "#tag" )({
       source: availableTags,
       select: function( event, ui ) {
         console.log(ui.item.link);
